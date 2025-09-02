b3c44a8a4298059cd1c43efbf3a469a818a1eba3
Robert Kirkman <rkirkman@termux.dev>
2025-09-02 03:42:45 -0500

    robertkirkman cinnamon temporary commit 1
    - apply `python-sass` to `mint-themes`
    
    - merge `0016-fix-python-package-location.patch` back into `fix-user-paths.diff`

--- x11-packages/mint-themes/build.sh
+++ x11-packages/mint-themes/build.sh
@@ -7,8 +7,17 @@ TERMUX_PKG_SRCURL=https://github.com/linuxmint/mint-themes/archive/refs/tags/${T
 TERMUX_PKG_SHA256=13ea29bdbf9efd62b45e3704e9bc36a45258f4a9b6fabbd3a46bf0a543dfd6d6
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-TERMUX_PKG_PYTHON_COMMON_DEPS="pysass"
+TERMUX_PKG_PYTHON_BUILD_DEPS="pysass"
+TERMUX_PKG_BUILD_DEPENDS="python-libsass"
 TERMUX_PKG_SUGGESTS="mint-x-icon-theme, mint-y-icon-theme"
+
+termux_step_pre_configure() {
+	# allow use of GNU/Linux pysass (TERMUX_PKG_PYTHON_BUILD_DEPS="pysass") during cross-compilation
+	# but bionic-libc pysass (TERMUX_PKG_BUILD_DEPENDS="python-sass") during on-device build
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		export PYTHONPATH="${TERMUX_PYTHON_CROSSENV_PREFIX}/cross/lib/python${TERMUX_PYTHON_VERSION}/site-packages"
+	fi
+}
 
 termux_step_make() {
 	cd $TERMUX_PKG_SRCDIR
